1757de50079e80ec9a4d4aa77d712080fab8ac49
CRXAhsan <ahsantariq669@gmail.com>
2023-08-24 23:26:22 +0500

    javascript

---
+++ Course 2/useOfArraysAndObjects.js
@@ -0,0 +1,21 @@
+var clothes = [];
+
+clothes.push("Shirt");
+clothes.push("Pants");
+clothes.push("T-Shirt");
+clothes.push("Belt");
+clothes.push("Shorts");
+
+clothes.pop();
+
+clothes.push("Shoes");
+
+console.log(clothes[3]);
+
+
+var favCar = {}
+
+favCar.color = " red";
+favCar.convertible = false;
+
+console.log(favCar);
