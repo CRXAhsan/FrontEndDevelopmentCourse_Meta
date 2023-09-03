58e93aa64b26b269de10e799648af62b578c0f09
CRXAhsan <ahsantariq669@gmail.com>
2023-09-03 23:55:21 +0500

    .

--- Course 2/defaultParameters.js
+++ Course 2/defaultParameters.js
@@ -10,19 +10,17 @@ function noDefaultParameters(number = 10) {
 
 // noDefaultParameters();
 
-class noDefaultParameter
-{
-    constructor(num1, num2, num3, string1, bool1)
-    {
+class noDefaultParameter {
+    constructor(num1, num2, num3, string1, bool1) {
         this.num1 = num1;
         this.num2 = num2;
         this.num3 = num3;
         this.string1 = string1;
-        this.bool1 = bool1; 
+        this.bool1 = bool1;
     }
-    
+
     calculate() {
-        if(this.bool1) {
+        if (this.bool1) {
             console.log(this.string1, this.num1 + this.num2 + this.num3);
             return;
         }
@@ -32,7 +30,7 @@ class noDefaultParameter
 
 }
 
-var fail = new noDefaultParameter(1,2,3,false);
+var fail = new noDefaultParameter(1, 2, 3, false);
 fail.calculate(); // 'The value of bool1 is incorrect'
 
 class WithDefaultParams {
@@ -44,7 +42,7 @@ class WithDefaultParams {
         this.bool1 = bool1;
     }
     calculate() {
-        if(this.bool1) {
+        if (this.bool1) {
             console.log(this.string1, this.num1 + this.num2 + this.num3);
             return;
         }
