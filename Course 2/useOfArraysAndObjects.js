c2ad1a6cfccca74b62f83f71a7bc1d417ee3ee78
CRXAhsan <ahsantariq669@gmail.com>
2023-09-07 19:38:24 +0500

    .

--- Course 2/useOfArraysAndObjects.js
+++ Course 2/useOfArraysAndObjects.js
@@ -13,9 +13,43 @@ clothes.push("Shoes");
 console.log(clothes[3]);
 
 
+
+
 var favCar = {}
 
 favCar.color = " red";
 favCar.convertible = false;
 
 console.log(favCar);
+
+const result = [];
+const drone = {
+    speed: 100,
+    color: 'yellow'
+}
+const droneKeys = Object.keys(drone);
+droneKeys.forEach(function (key) {
+    result.push(key, drone[key])
+})
+console.log(result)
+
+
+//these are the three methods for array 
+//1. foreach
+//2. map
+//3. nums
+
+const fruits = ['kiwi', 'mango', 'apple', 'pear'];
+function appendIndex(fruit, index) {
+    console.log(`${index}. ${fruit}`)
+}
+fruits.forEach(appendIndex);
+
+const nums = [0, 10, 20, 30, 40, 50];
+nums.filter(function (num) {
+    return num > 20;
+});
+
+[0, 10, 20, 30, 40, 50].map(function (num) {
+    return num / 10
+});
