b3f0707f0a737ddea07580b9c4a4c9f622774608
CRXAhsan <ahsantariq669@gmail.com>
2023-09-16 19:46:21 +0500

    .

--- Course 2/Assignment No 3/solution.js
+++ Course 2/Assignment No 3/solution.js
@@ -16,7 +16,6 @@ logDairy();
 // Create a function called logDairy. Within it, console log each of the items in the dairy array, using the for...of loop.
 
 // After you create this function, call it as logDairy() to see the output on the console.
-
 // The expected output should be:
 
 // cheese
